f9c5b26c5d861a515dd53d7bcf0bab4975ec1a05
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-01-13 20:17:18 +0100

    Updates

---
+++ precache-manifest.3b80659bd3e2b371e3c771ce8f47d944.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4303adab6b846af99bd2b22aedfd350d",
+    "url": "/exam/index.html"
+  },
+  {
+    "revision": "f2bd97866ac529456f40",
+    "url": "/exam/static/css/2.2ec3db0d.chunk.css"
+  },
+  {
+    "revision": "2df125c0ada494329d4e",
+    "url": "/exam/static/css/main.d2b2d954.chunk.css"
+  },
+  {
+    "revision": "f2bd97866ac529456f40",
+    "url": "/exam/static/js/2.42371ebf.chunk.js"
+  },
+  {
+    "revision": "f2d06b9d254fdbb1a644de9b1a92e5e8",
+    "url": "/exam/static/js/2.42371ebf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2df125c0ada494329d4e",
+    "url": "/exam/static/js/main.f0956ed4.chunk.js"
+  },
+  {
+    "revision": "18a701581c6bc0d89033",
+    "url": "/exam/static/js/runtime-main.6bdc4d1f.js"
+  }
+]);
